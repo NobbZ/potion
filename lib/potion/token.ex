dcee056b2a24a610657037add021a8ed4d14770d
Norbert Melzer <timmelzer@gmail.com>
2015-04-27 18:43:52 +0200

    Drastically reorganized Potion.Token

--- lib/potion/token.ex
+++ lib/potion/token.ex
@@ -60,61 +60,33 @@ defmodule Potion.Token do
 
       iex(5)> Potion.Token.valid? %Potion.Token{type: :string, value: 12.0} 
       false
+
+      iex(6)> Potion.Token.valid? %Potion.Token{type: :operator, value: "+"}
+      true
+
+      iex(7)> Potion.Token.valid? %Potion.Token{type: :operator, value: "a"}
+      false
+
+      iex(8)> Potion.Token.valid? %Potion.Token{type: :operator, value: "|+|"}
+      true
   """
   @spec valid?(any) :: boolean
-  def valid?(tok = %Potion.Token{}) do
-    if Enum.find(@types, false, fn(x) -> x == tok.type end) do
-      case tok.type do
-        :keyword  -> valid_keyword?(tok)
-        :int      -> valid_int?(tok)
-        :string   -> valid_string?(tok)
-        :float    -> valid_float?(tok)
-        :operator -> valid_operator?(tok)
-        _         -> false
-      end
+  def valid?(%__MODULE__{type: :int,     value: val}) when is_integer(val),    do: true
+  def valid?(%__MODULE__{type: :float,   value: val}) when is_float(val),      do: true
+  def valid?(%__MODULE__{type: :string,  value: val}), do: String.valid?(val)
+  def valid?(%__MODULE__{type: :keyword, value: val}) do
+    if String.valid?(val) do
+      Enum.find(@keywords, false, &(&1 == val))
+    else
+      false
     end
   end
-
+  def valid?(%__MODULE__{type: :operator, value: val}) do
+    if String.valid?(val) do
+      String.codepoints(val) |> Enum.all?(fn(cp) ->
+        String.contains? @op_chars, cp
+      end)
+    end
+  end
   def valid?(_), do: false
-
-  defp valid_keyword?(tok) do
-    case tok.type do
-      :keyword -> Enum.find(@keywords, false, fn(x) -> x == tok.value end)
-      _        -> false
-    end
-  end
-
-  defp valid_int?(tok) do
-    case tok.type do
-      :int -> is_integer tok.value
-      _    -> false
-    end
-  end
-
-  defp valid_string?(tok) do
-    case tok.type do
-      :string -> String.valid? tok.value
-      _       -> false
-    end
-  end
-
-  defp valid_float?(tok) do
-    case tok.type do
-      :float -> is_float tok.value
-      _      -> false
-    end
-  end
-
-  defp valid_operator?(tok) do
-    case tok.type do
-      :operator -> String.valid?(tok.value) && only_op_chars?(tok.value)
-      _         -> false
-    end
-  end
-
-  defp only_op_chars?(op_string) do
-    String.codepoints(op_string) |> Enum.all(fn(cp) ->
-      String.contains? @op_chars, cp
-    end)
-  end
 end
